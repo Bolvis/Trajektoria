81c67aaafeac79ee9ecacf8805403741564b4c45
Dawid <dawid.stasiak17@gmail.com>
2020-06-09 13:45:10 +0200

    Initial Commit

---
+++ Trajektoria/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Trajektoria
+//
+//  Created by Dawid Stasiak on 09/06/2020.
+//  Copyright © 2020 Dawid Stasiak. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
